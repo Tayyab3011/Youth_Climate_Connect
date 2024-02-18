d27085c84cdc3acbaae77822882e9b2873d2ec98
Muhammad Tayyab <tayyabahmad031@gmail.com>
2024-02-19 01:40:11 +0500

    My First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY>50) {
+        navbar.classList.add('sticky')
+    } else {
+        
+        navbar.classList.remove('sticky')
+    }
+});
